f5f993256a81a1b0ad2bce4d7199c7981e918c0a
DanielDCampos <daniel.adsistemas@gmail.com>
2025-06-19 02:50:15 -0300

    QUARTO COMMIT COM HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("ola");
+const heroi = "cruze";
+
+let xp = 25000; // Exemplo de valor numérico
+
+if (xp < 1000) {
+  console.log("ferro");
+} else if (xp <= 2000) {
+  console.log("cobre");
+} else if (xp <= 3000) {
+  console.log("bronze");
+} else if (xp <= 4000) {
+  console.log("Prata");
+} else if (xp <= 5000) {
+  console.log("Ouro");
+} else if (xp <= 6000) {
+  console.log("Diamante");
+} else if (xp <= 7000) {
+  console.log("Super Char");
+} else if (xp <= 8000) {
+  console.log("Rubi");
+} else if (xp <= 9000) {
+  console.log("Mestre dos Play");
+} else if (xp <= 1000) {
+  console.log("MASTER DOS MASTERS");
+} else {
+  console.log("nível mais alto");
+}
+console.log("o " + heroi + " esta no nivel " + xp);
